162a0e16f588464a1ae454a6edbb6d027ed61ef1
DylanMumaw <mumaw0@pnw.edu>
2018-11-27 16:32:27 -0600

    Add files via upload

---
+++ Profile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace CNIT355_Final_Project
+{
+    public class Profile : ContentPage
+    {
+        public Profile()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Welcome to Xamarin.Forms!" }
+                }
+            };
+        }
+    }
+}
---
+++ Venue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+namespace CNIT355_Final_Project
+{
+    class Venue
+    {
+        //class for each Venue, Artist, Manager set up for database interactiability
+
+        [PrimaryKey, AutoIncrement]
+        public int VenueID { get; set; }
+
+        [MaxLength(30)]
+        public string VenueName { get; set; }
+
+        [MaxLength(2)]
+        public string VenueState { get; set; }
+
+        [MaxLength(30)]
+        public string VenueCity { get; set; }
+
+        [MaxLength(50)]
+        public string VenueAddress { get; set; }
+
+        [MaxLength(5)]
+        public string VenueZIP { get; set; }
+    }
+}
