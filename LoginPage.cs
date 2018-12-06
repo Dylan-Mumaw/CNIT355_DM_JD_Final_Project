12c6a44107fbed1972dcc2aa23278e90a1d33cef
DylanMumaw <mumaw0@pnw.edu>
2018-12-06 11:10:38 -0600

    Add files via upload

--- LoginPage.cs
+++ LoginPage.cs
@@ -4,13 +4,20 @@ using System.Linq;
 using System.Text;
 
 using Xamarin.Forms;
+using SQLite;
 
 namespace CNIT355_Final_Project
 {
 	public class LoginPage : ContentPage
 	{
-		public LoginPage ()
+        protected SQLiteConnection myDatabase;
+        public string currentUser;
+        public string userType;
+        public LoginPage ()
 		{
+            myDatabase = DependencyService.Get<IDatabase>().ConnectToDB();
+            myDatabase.CreateTable<User>();
+            var user1 = new User{FirstName = "Dylan", LastName = "Mumaw", Username = "DylanMumaw", Type = "Artist"};
             Label homeLabel = new Label
             {
                 Text = "Welcome to FAME's Artist Managment System, please sign in below ",
@@ -21,8 +28,7 @@ namespace CNIT355_Final_Project
 
             Entry nameEntry = new Entry
             {
-                Text = "Enter Username" //will only be artist or manager nothing too complex
-                
+                Placeholder = "Enter Username" //will only be artist or manager nothing too complex                
             };
 
             Entry passEntry = new Entry
@@ -33,6 +39,27 @@ namespace CNIT355_Final_Project
             Button loginButton = new Button
             {
                 Text = "Login"
+            };
+            loginButton.Clicked += (sendernav, args) =>
+            {
+               currentUser = myDatabase.Query<User>("SELECT Username FROM User WHERE Username = " + "'" + nameEntry.Text.ToString() + "'").ToString();
+               userType = myDatabase.Query<User>("SELECT Type FROM User WHERE Username = " + "'" + currentUser + "'").ToString();
+               if(currentUser == null)
+               {
+                    DisplayAlert("Error", "Create an account to continue.", "OK");
+               }
+               else if (userType == "Venue")
+               {
+                    Navigation.PushAsync(new VenueInfoPage());
+               }
+               else if (userType == "Manager")
+               {
+                    Navigation.PushAsync(new ManagerInfoPage());
+               }
+               else if (userType == "Artist")
+                {
+                    Navigation.PushAsync(new ArtistInfoPage());
+                }
             };
 
             Button accountButton = new Button
